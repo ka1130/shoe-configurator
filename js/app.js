851b52267a5c34d72a8f12c2154e6acb7a2e7b82
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-08-18 15:36:44 +0200

    Change shoe on radion change

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 (function() {
     document.addEventListener("DOMContentLoaded", function(event) {
-        console.log("ok");
+
+        const shoeImg = document.getElementById("shoe-img").children[0];
+        const radioColor = document.getElementsByName("color");
+
+        const toeImg = document.getElementById("toe-img").children[0];
+        const radioToe = document.getElementsByName("toe");
+
+
+        function changeColor(event) {
+            event.preventDefault();
+            shoeImg.src = "img/" + this.dataset.img;
+        }
+
+        function changeToe(event) {
+            event.preventDefault();
+            toeImg.src = "img/" + this.dataset.img;
+        }
+
+        for (let i = 0; i < radioColor.length; i++) {
+            radioColor[i].addEventListener("change", changeColor, false);
+        }
+
+        for (let i = 0; i < radioToe.length; i++) {
+            radioToe[i].addEventListener("change", changeToe, false);
+        }
+
     });
 })();
